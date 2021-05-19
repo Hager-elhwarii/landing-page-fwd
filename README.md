bec97f48eaed294a252147248642f05257ef390f
hager-dot <hagerelhwarii6633@gmail.com>
2021-05-19 20:06:05 +0200

    add js and styling

---
+++ README.md
@@ -0,0 +1,24 @@
+#  Coffee Lab - Simple Landing Page
+
+## Table of contents 
+
+-[General info](#general-info)
+-[Technologies](#technologies)
+-[Setup](#setup)
+
+### General info
+
+This project is a simple landing page for 
+the udacity program project.
+
+### Technologies
+
+Project is created with:
+
+- HTML version: 5
+- Javascript version: 1.7
+
+### Setup 
+
+To run this project, install it locally
+either by cloning or downloading.
---
+++ js/app.js
@@ -0,0 +1,104 @@
+const navSections = document.querySelectorAll('section')
+const navBar = document.getElementById('links')
+
+//DYNAMIC NAV BAR
+function dynamivNavBar() {
+  let fragment = document.createDocumentFragment()
+  navSections.forEach((element) => {
+    let newli = document.createElement('li')
+    let newLink = document.createElement('a')
+    const elementId = element.id
+    // console.log(elementId)
+    let navLink = elementId
+      .split('-')
+      .map((word) => capitalize(word))
+      .join(' ')
+    let txtContent = document.createTextNode(navLink)
+    let attr = document.createAttribute('data-link')
+    let hrefAttr = document.createAttribute('href')
+    attr.value = `#${element.id}`
+    hrefAttr.value = `#${element.id}`
+    newLink.setAttributeNode(attr)
+    newLink.setAttributeNode(hrefAttr)
+
+    let attr1 = document.createAttribute('class')
+    attr1.value = 'nav-link'
+    newLink.setAttributeNode(attr1)
+    newLink.appendChild(txtContent)
+    // console.log(txtContent)
+    newli.appendChild(newLink)
+    // console.log(newli)
+    fragment.appendChild(newli)
+    helper()
+  })
+  console.log(fragment)
+  navBar.appendChild(fragment)
+
+}
+function helper() {
+  window.addEventListener('DOMContentLoaded', (event) => {
+    let mylinks = document.querySelectorAll('.link')
+    mylinks.forEach((Elm) => {
+      Elm.addEventListener('click', function (event) {
+        let sectionid = event.target.textContent
+        let getit = document.getElementById(sectionid)
+        getit.scrollIntoView()
+      })
+    })
+    console.log('DOM fully loaded and parsed')
+  })
+}
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+function inViewPort(element) {
+  const dimensions = element.getBoundingClientRect()
+  const top = dimensions.top
+  const left = dimensions.left
+  const bottom = dimensions.bottom
+  const right = dimensions.right
+
+  return (
+    top >= 0 &&
+    left >= 0 &&
+    bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
+function activeLinkFunction() {
+  const navLinks = document.querySelectorAll('.nav-link')
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      const activeLink = document.querySelector('.nav-link.active')
+      if (activeLink) {
+        activeLink.classList.remove('active')
+      }
+      const currentLink = event.target
+      currentLink.classList.add('active')
+    })
+  })
+}
+
+//SELECT ACTIVE SECTION
+
+function activeSection() {
+  console.log(navSections)
+  document.addEventListener('scroll', function (event) {
+    event.preventDefault()
+    navSections.forEach((section) => {
+      if (inViewPort(section)) {
+        section.classList.add('highlighted')
+      }
+    })
+  })
+}
+
+function main() {
+  dynamivNavBar()
+  activeLinkFunction()
+  activeSection()
+}
+main()
+// console.log(navBar)
